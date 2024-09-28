5b59f87f5b6ce2930af5d9ea4136cef974b1de63
Temitope <tpsolesi@gmail.com>
2024-09-28 09:28:47 +0100

    Update utils.py
    Adjust Selenium options to soothe heroku

--- file/utils.py
+++ file/utils.py
@@ -114,7 +114,7 @@ def loads_urls(urls):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--remote-debugging-port=0")
     options.add_argument("start-maximized")
     options.add_argument("enable-automation")
     options.add_argument("--disable-infobars")
