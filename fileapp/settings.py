e4dd9d6ecf904461296e244ff4f840874e8b7eb6
Temitope <tpsolesi@gmail.com>
2024-11-14 11:34:31 +0100

    Update settings.py

--- fileapp/settings.py
+++ fileapp/settings.py
@@ -103,27 +103,27 @@ AUTH_USER_MODEL = 'authentication.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#   DATABASES = {
-#       'default': {
-#           'ENGINE': 'django.db.backends.sqlite3',
-#           'NAME': BASE_DIR / 'db.sqlite3',
-#   }
-#
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': '127.0.0.1',
-        'NAME': 'fileapp',
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS'),
-        'PORT': 5432,
-        'OPTIONS': {
-            'sslmode': 'require',
-        }
-    }
+       'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+   }
 }
+
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'HOST': '127.0.0.1',
+#        'NAME': 'fileapp',
+#        'USER': os.getenv('DB_USER'),
+#        'PASSWORD': os.getenv('DB_PASS'),
+#        'PORT': 5432,
+#        'OPTIONS': {
+#            'sslmode': 'require',
+#        }
+#    }
+#}
 
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
