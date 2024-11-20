7aaf6b78eda68a504a5b11cf785fba1ab805ec07
Temitope <tpsolesi@gmail.com>
2024-11-20 22:47:45 +0100

    Update views.py

--- file/views.py
+++ file/views.py
@@ -15,7 +15,6 @@ from django.urls import reverse
 from django.http import JsonResponse
 from django.conf import settings
 from .models import Document, ChatMessage, Project
-
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponseBadRequest, JsonResponse
